6d69700891107e3e2de7fc1c895123023a70d2c2
antoinemoyet <antoinemoyet33@gmail.com>
2017-11-17 10:21:38 +0100

    add favorite

--- src/components/Bookmark.js
+++ src/components/Bookmark.js
@@ -1,8 +1,33 @@
-import React, {component} from 'react';
+import React, {Component} from 'react';
 import SampleScreen from './SampleScreen';
 import Unsplash from 'unsplash-js/native';
-import {AppRegistry, StyleSheet, View, Image, Text, TouchableWithoutFeedback, Dimensions, Modal, } from 'react-native';
+import { AppRegistry, StyleSheet, View, Image, Text, TouchableWithoutFeedback, Dimensions, Modal, } from 'react-native';
 
-const Bookmark = () => <SampleScreen title={'🍺'} />
+const Bookmark = () => <SampleScreen title={''} />
+
+class bookmark extends Component {
+     constructor(props) {
+		super(props);
+		let favoritebookmarkData = [];
+		favoritebookmarkData.push(this.props.topicNames)			
+		
+		this.state = {
+			favoritebookmarkData: favoritebookmarkData
+		};
+	}
+     componentWillReceiveProps(nextProps){
+		let favoritebookmarkData = [];
+		favoritebookmarkData.push(this.props.topicNames)
+					
+		this.setState({favoritebookmarkData});
+	}
+     render() {
+			return (
+				<View key={"Bookmark"}>
+					<ImageElement imgsourceData={this.state.favoritebookmarkData} openSubCat={this._openSubCat} parentKey={"Bookmark"} />
+				</View>
+			);
+		}
+ }
 
 export default Bookmark;
